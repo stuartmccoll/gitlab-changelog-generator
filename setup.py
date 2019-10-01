742d886abd3a5dd21ce86c8c5a0f9abadd34e385
Stuart McColl <contact@stuartmccoll.co.uk>
2019-10-01 20:37:50 +0100

    Added flag to disable SSL certificate checking (#31)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gitlab-changelog-generator",
-    version="1.0.5",
+    version="1.0.6",
     author="Stuart McColl",
     author_email="it@stuartmccoll.co.uk",
     description="A small command line utility for generating CHANGELOG.md "
