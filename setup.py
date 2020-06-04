7fb7bca3ded370d5c442d70b01100ba1fa5efa87
NghiaLT <nghialt.11@gmail.com>
2020-06-04 10:47:10 +0700

    chg: update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "console_scripts": ["changegen=changelog_generator.entry_point:main"]
     },
     packages=setuptools.find_packages(),
-    install_requires=["requests", "python-dateutil"],
+    install_requires=["requests", "python-dateutil", "iso8601", "rfc3339"],
     tests_require=["unittest", "mock"],
     classifiers=(
         "Environment :: Console",
